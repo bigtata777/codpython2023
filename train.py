022dc28d801a82b12997df272935d2afee657316
beto <estadistico1984gmail.com>
2024-04-30 00:02:48 -0400

    vuelvo de un reset

--- train.py
+++ train.py
@@ -1,1 +1,9 @@
 print("hello world")
+import numpy as np
+numeros =list(range(100+1))
+funcion = lambda x: (125*x**2 + 2*x  + 0.9*x + 5)/(np.cos(x*3)+100*x + x**5)
+output = []
+
+for i in numeros:
+    output.append(funcion(i))
+print(output)
