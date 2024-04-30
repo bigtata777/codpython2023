1782f91991e9291e7c2df8bcfd39d18cf488c282
beto <estadistico1984gmail.com>
2024-04-30 09:38:13 -0400

    mas modificaciones a la funcion

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 print("hello world")
 import numpy as np
-numeros =list(range(100+1))
-funcion = lambda x: (125*x**2 + 2*x  + 0.9*x + 5)/(np.cos(x*3)+100*x + x**5)
+numeros =list(range(200+1))
+funcion = lambda x:  (np.sqrt(x) + x**2) + (125*x**2 + 2*x  + 0.9*x + 5)/(np.cos(x*3)+100*x + x**5)
 output = []
 
 for i in numeros:
