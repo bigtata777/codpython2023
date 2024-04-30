4ea6495861925e9e34ea26ffe4b5dfbb7b33e727
beto <estadistico1984gmail.com>
2024-04-30 13:18:00 -0400

    genero secuencia random numbers

--- train.py
+++ train.py
@@ -1,6 +1,8 @@
 print("hello world")
 import numpy as np
-numeros =list(range(200+1))
+import random
+
+numeros =[random.random() for _ in range(100)]
 funcion = lambda x:  (np.sqrt(x) + x**2) + (125*x**2 + 2*x  + 0.9*x + 5)/(np.cos(x*3)+100*x + x**5)
 output = []
 
