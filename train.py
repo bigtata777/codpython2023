76af2655f63554624693c8362feb81a053d28813
beto <estadistico1984gmail.com>
2024-04-30 15:53:27 -0400

    agrego otra funcion

--- train.py
+++ train.py
@@ -3,9 +3,13 @@ import numpy as np
 import random
 
 numeros =[random.random() for _ in range(100)]
-funcion = lambda x:  (np.sqrt(x) + x**2) + (125*x**2 + 2*x  + 0.9*x + 5)/(np.cos(x*3)+100*x + x**5)
-output = []
+funcion1 = lambda x:  (np.sqrt(x) + x**2) + (125*x**2 + 2*x  + 0.9*x + 5)/(np.cos(x*3)+100*x + x**5)
+funcion2 = lambda x: (x**2- 0.8*x - 1)/(120*x**2)
+output1 = []
+output2 = []
 
 for i in numeros:
-    output.append(funcion(i))
-print(output)
+    output1.append(funcion1(i))
+    output2.append(funcion2(i))
+print(output1)
+print(output2)
